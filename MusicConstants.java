8034be192c4f603ec399bc4e134e32f67de3b369
Wilson Chen <36680838+KeroseneTrampolines@users.noreply.github.com>
2022-10-10 11:42:39 -0400

    Refactor Main.java, add MusicConstants

---
+++ MusicConstants.java
@@ -0,0 +1,12 @@
+public class MusicConstants {
+    public static final int WHOLE_NOTE = 0;
+    public static final int HALF_NOTE = 1;
+    public static final int QUARTER_NOTE = 2;
+    public static final int EIGHTH_NOTE = 3;
+    public static final int SIXTEENTH_NOTE = 4;
+
+    public static final int SYMBOL_NOTE = 0;
+    public static final int SYMBOL_REST = 1;
+    public static final int SYMBOL_FLAT = 2;
+    public static final int SYMBOL_SHARP = 3;
+}
